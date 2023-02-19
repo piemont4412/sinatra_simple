c00b294200e8c25a6198c4dfa9de3c2f4407b912
piemont4412 <piemont4412@dream.jp>
2023-02-19 10:02:04 +0900

    画像をアップロードするための変更

--- main.rb
+++ main.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
+require 'sqlite3'
+require 'securerandom'
+
+db = SQLite3::Database.new "db/post.db"
+db.results_as_hash = true
 
 get '/' do
-  erb :index
+  posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
+  erb :index, { :locals => { :posts => posts } }
 end
 
 get '/hello' do
@@ -10,4 +16,34 @@ end
 
 get '/example' do
   erb :example
+end
+
+post '/' do
+  file_name = ""
+
+  if params["file"]
+    ext = ""
+    if params["file"][:type].include? "jpeg"
+      ext = "jpeg"
+    elsif params["file"][:type].include? "png"
+      ext = "png"
+    else
+      return "投稿できる画像形式はJPEGとPNGだけです"
+    end
+
+    #適当なファイル名を付ける
+    file_name = SecureRandom.hex + "." + ext
+
+    #画像を保存
+    File.open("./public/uploads/" + file_name, "wb") do |f|
+      f.write params["file"][:tempfile].read
+    end
+  else
+    return "画像が必須です"
+  end
+
+  stmt.db("INSERT INTO posts (text, img_file_name) VALUES (?, ?)")
+  stmt.bind_params(params["ex_text"], file_name)
+  stmt.execute
+  redirect '/'
 end
